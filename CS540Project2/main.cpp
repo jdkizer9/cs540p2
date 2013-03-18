b7b5a2d902326ee8f031d84cc7829d091fbd72bd
James <james.kizer@gmail.com>
2013-03-18 06:54:00 -0400

    First directory tree commit

---
+++ CS540Project2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS540Project2
+//
+//  Created by James Kizer on 3/18/13.
+//  Copyright (c) 2013 James Kizer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
